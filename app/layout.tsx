856a57dc27cd942e7220e0c61e3d8517b09852b3
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-10-27 14:48:46 +0100

    reapring scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Esencja | Agencja Marketingowa",
+  title: "Esencja | Kreatorzy reklam",
   description: "Profesjonalna agencja marketingowa, która wyniesie Twój biznes na wyższy poziom.",
 };
 
