5619cb3d35866dc95ae783413fe8650b59221c31
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-10-27 14:36:06 +0100

    tested build

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,9 +24,9 @@ const Hero = () => {
             <div className={'flex justify-center relative my-20 z-10'}>
                 <div className={'max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'}>
                     <h2 className={'uppercase tracking-widest text-xs text-center text-black-100 max-w-80'}>
-                        Przenieś swoje reklamy na nowy poziom
+                        WYDAJESZ NA REKLAMĘ, ALE NIE ZARABIENIASZ?
                     </h2>
-                    <TextGenerateEffect numberOf={6} className={'text-center text-[40px] md:text-5xl lg:text-6xl'} words={`Bądź w centrum uwagi przez cały rok. Zbuduj nieśmiertelny markę.`} />
+                    <TextGenerateEffect numberOf={6} className={'text-center text-[40px] md:text-5xl lg:text-6xl'} words={`Bądź w centrum uwagi przez cały rok. Zbuduj nieśmiertelną markę.`} />
                     <p className={'text-center md:tracking-wider mb-2 text-sm md:text-lg lg:text-2xl mt-2.5'}>Esencja | Kreator Reklam</p>
 
                     <a href={'#about'}>
